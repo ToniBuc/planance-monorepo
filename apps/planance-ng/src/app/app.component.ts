edf013d596b6a01a248f88810520357cf7558f0a
Toni Buconjic <dyzzles@hotmail.com>
2024-06-10 14:51:45 +0200

    restructure routing and temporarily remove theming

--- apps/planance-ng/src/app/app.component.ts
+++ apps/planance-ng/src/app/app.component.ts
@@ -1,27 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { LoginComponent } from './components/login/login.component';
-import { ThemeToggleComponent } from './components/theme-toggle/theme-toggle.component';
-import { RegisterComponent } from './components/register/register.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, HeaderComponent, LoginComponent, RegisterComponent, ThemeToggleComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'planance';
-  public showPlaceholder = true;
-
-  public togglePlaceholder(event: Component): void {
-    if (event instanceof LoginComponent || event instanceof RegisterComponent) {
-      this.showPlaceholder = false;
-    } else {
-      this.showPlaceholder = true;
-    }
-  }
 }
--- apps/planance-ng/src/app/components/header/header.component.ts
+++ apps/planance-ng/src/app/components/header/header.component.ts
@@ -1,32 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { ThemeToggleService } from '../theme-toggle/theme-toggle.service';
-import { Theme } from '../theme-toggle/theme-toggle.model';
-import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [ThemeToggleComponent]
+  imports: []
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
   logoSrc = '';
 
-  constructor(
-    private themeToggleService: ThemeToggleService
-  ) { }
-
-  public async ngOnInit() {
-    await this.themeToggleService.themeChanged1$.subscribe(() => {
-      if (this.themeToggleService.getCurrentTheme() === Theme.DARK) {
-        this.logoSrc = "../../assets/thatsanorder-white.png";
-      }
-      else {
-        this.logoSrc = "../../assets/thatsanorder-black.png";
-      }
-    })
-  }
+  constructor() {}
   
 }
